10efac331a132a191ff70e4f8df6121bf70784e0
Cody <codywscw@gmail.com>
2023-06-20 07:56:39 -0500

    build script fixes

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,7 +4,7 @@ param($version)
 Set-Location ..
 
 # Call the increment_version script
-.\scripts\increment_version.ps1 -version $version
+.\scripts\Increment-Version.ps1 -version $version
 
 # Create the zip file
 Compress-Archive -Path assets, LICENSE, manifest.json, popup.html, popup.js, tabGrouper.js, readme.md -DestinationPath .\releases\tabgrouper_$version.zip

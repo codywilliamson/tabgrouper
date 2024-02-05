9303fa3cf35d405e39060ab440fed500a9f51f6b
Cody <codywscw@gmail.com>
2024-02-05 09:56:55 -0600

    plasmo init

--- scripts/build.ps1
+++
@@ -1,7 +0,0 @@
-param($version)
-
-# Call the increment_version script
-.\scripts\Increment-Version.ps1 -version $version
-
-# Create the zip file
-Compress-Archive -Path assets, LICENSE, manifest.json, popup.html, popup.js, tabGrouper.js, readme.md -DestinationPath .\releases\tabgrouper_$version.zip
